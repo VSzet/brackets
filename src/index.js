e2eb0b852b229b8ac328367fb6a3b9f43a01a1e6
Budosinobi <61315473+Budosinobi@users.noreply.github.com>
2021-03-11 09:33:46 +0300

    feat: added code index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let sub = [];
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            sub[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+        }
+
+        for (let k = 0; k < sub.length; k++) {
+            if (str.includes(sub[k])) {
+                str = str.replace(sub[k], "");
+                k = -1;
+            }
+        }
+    }
+    if (str === "") {
+        return true;
+    }
+    return false;
+};
